f5fccd9703325c5079b744ac90ad3e51a5ba4c67
Informatica <informatica@alumno.local>
2025-09-02 15:40:50 -0600

    Creacion de repo

---
+++ src/main/java/org/jrae/rentavehiculos/RentavehiculosApplication.java
@@ -0,0 +1,13 @@
+package org.jrae.rentavehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentavehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentavehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jrae/rentavehiculos/RentavehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrae.rentavehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentavehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

953882f9e3db741a31e867a2aa3927226f0ecd87
Informatica <informatica@alumno.local>
2025-09-04 14:51:15 -0600

    Cambios en UsuarioCrud

--- src/main/java/org/jrae/rentavehiculos/persistence/crud/UsuarioCurd.java
+++ src/main/java/org/jrae/rentavehiculos/persistence/crud/UsuarioCurd.java
@@ -2,5 +2,8 @@ package org.jrae.rentavehiculos.persistence.crud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioCurd  extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByCorreo(String correo);
 }
